e79a283f52719b2959783b82ca58b07fd61ce299
Vu <minhang.vu.03@gmail.com>
2020-09-22 17:41:02 +0200

    move form to separate file and include it to each page

---
+++ form.php
@@ -0,0 +1,29 @@
+
+<form>
+            <div class="abstand">
+                <label for="Date">Date 
+                <input type="date" required>
+                </label>
+                <label for="amount">Amount
+                <input type="number" min="0.05" value="0.00"  minlength="3" required >
+                </label>
+                <label for="category">Category
+                <input list="browsers" name="category" required>
+                </label>
+                <datalist id="browsers">
+                    <option value="Haushalt">
+                    <option value="Verpflegung">
+                    <option value="Freizeit, Sport & Hobby">
+                    <option value="Kommunikation & Medien">
+                    <option value="Verkehr, Auto & Transport">
+                    <option value="Persönliche Ausgaben">
+                    <option value="Gesundheit">
+                    <option value="Sonstige Ausgaben">
+                    </option>
+                </datalist>
+            </div>
+            <div class="abstand-button">
+                <button>Clear</button>
+                <button>Submit</button>
+            </div>
+        </form>
--- update.php
+++ update.php
@@ -15,34 +15,9 @@
         </header>
         <br>
     <main class="abstand-header">
-        <form>
-            <div class="abstand">
-            <label for="Date">date 
-            <input type="date" required>
-            </label>
-            <label for="amount">amount
-            <input type="number" min="0.05" value="0.00"  minlength="3" required >
-            </label>
-            <label for="category">category
-            <input list="browsers" name="category" required>
-            </label>
-            <datalist id="browsers">
-                <option value="Haushalt">
-                <option value="Verpflegung">
-                <option value="Freizeit, Sport & Hobby">
-                <option value="Kommunikation & Medien">
-                <option value="Verkehr, Auto & Transport">
-                <option value="Persönliche Ausgaben">
-                <option value="Gesundheit">
-                <option value="Sonstige Ausgaben">
-                </option>
-            </datalist>
-            </div>
-            <div class="abstand-button">
-            <button>clear</button>
-            <button>create</button>
-          </form>
-          </div>
+        <?php
+            include "form.php"
+        ?>
     </main>
 </body>
 </html>
